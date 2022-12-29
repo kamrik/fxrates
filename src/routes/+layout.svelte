09f16cc98b88427d1482d222c21ca3bd665cbad9
Mark Koudritsky <kamrik@gmail.com>
2022-12-29 21:00:19 +0100

    Add picocss demo page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '@picocss/pico';
+	import '../app.css';
+</script>
+
+<nav class="container">
+	<ul><li><strong>Logo</strong></li></ul>
+	<ul>
+		<li><a href="/">Item 1</a></li>
+		<li><a href="pico">Pico</a></li>
+	</ul>
+</nav>
+<slot />
+
+<footer class="container">
+	<hr />
+	<p>Footer be here</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	function handleClick() {
+		console.log('Clicked');
+	}
+</script>
+
+<main class="container">
+	<h2>Main title</h2>
+	<button class="contrast theme-switcher">…</button>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<p>Press <kbd>Ctrl+Z</kbd> to fix <s>shit</s> it all</p>
+	<button on:click={handleClick}> Click me</button>
+</main>
+
+<style>
+	main {
+		/* For alternating dark and light blocks */
+		background-color: var(--background-color);
+	}
+</style>
