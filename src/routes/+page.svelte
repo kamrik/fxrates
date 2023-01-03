21ce066b00382db82ebdaf2cb64b3add4108f6b1
Mark Koudritsky <kamrik@gmail.com>
2023-01-03 20:36:02 +0100

    A working version with static local JSON file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,58 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let loading = true;
+	let rateMap = new Map();
+	let rates: any[] = [];
+
+	let amount1 = 1;
+	let amount2 = 1;
+
+	// TODO: try to get the default pair from local storage or cookie
+	let symbol1 = 'USD';
+	let symbol2 = 'RUB';
+
+	// $: amount2 = (amount1 / rateMap.get(symbol1).rate) * rateMap.get(symbol2).rate;
+	$: amount2 = convert(amount1, symbol1, symbol2);
+
+	onMount(async () => {
+		const url = '/rates.json';
+		const res = await fetch(url);
+		rates = await res.json();
+		rateMap = new Map();
+		rates.forEach((rate: any) => {
+			rateMap.set(rate.symbol, rate);
+		});
+
+		loading = false;
+
+		amount2 = convert(amount1, symbol1, symbol2);
+	});
+
+	function swap() {
+		const symTmp = symbol2;
+		symbol2 = symbol1;
+		symbol1 = symTmp;
+	}
+
+	function convert(amount: number, fromSymbol: string, toSymbol: string) {
+		if (loading) return NaN;
+
+		const fromRate = rateMap.get(fromSymbol)?.rate;
+		const toRate = rateMap.get(toSymbol)?.rate;
+		let x = (amount / fromRate) * toRate;
+		// Rounding
+		if (x >= 1) {
+			return Math.round(x * 100) / 100;
+		}
+
+		// For numbers below 1, preserve N significant digits
+		const N = 4;
+		const pow = Math.floor(Math.log10(x));
+		const factor = Math.pow(10, N - 1 - pow);
+		return Math.round(x * factor) / factor;
+	}
+
 	function handleClick() {
 		console.log('Clicked');
 	}
@@ -6,15 +60,72 @@
 
 <main class="container">
 	<h2>Main title</h2>
-	<button class="contrast theme-switcher">…</button>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+	<input type="number" bind:value={amount1} />
+
+	{#if loading}
+		<span aria-busy="true" />
+	{:else}
+		<div class="currencies">
+			<div>
+				<select id="cur1select" name="cur1" bind:value={symbol1}>
+					{#each rates as rate, i}
+						<option value={rate.symbol}>{rate.symbol}</option>
+					{/each}
+				</select>
+
+				<select id="cur2select" name="cur2" bind:value={symbol2}>
+					{#each rates as rate}
+						<option value={rate.symbol}>{rate.symbol}</option>
+					{/each}
+				</select>
+			</div>
+			<button on:click={swap}>Swap</button>
+		</div>
+
+		<p>
+			<b>{amount1}</b>
+			{symbol1} =
+			<b>{amount2}</b>
+			{symbol2}
+		</p>
+		<div class="small-rates">
+			<span>1 {symbol1} = {convert(1, symbol1, symbol2)} {symbol2}</span>
+			<span>1 {symbol2} = {convert(1, symbol2, symbol1)} {symbol1}</span>
+		</div>
+	{/if}
+
+	<!-- Raw data -->
+	<article>
+		<header>
+			<h3>Raw data</h3>
+		</header>
+		<pre>{JSON.stringify(rates, null, '    ')}</pre>
+	</article>
+
+	<!-- Experiments -->
 	<p>Press <kbd>Ctrl+Z</kbd> to fix <s>shit</s> it all</p>
-	<button on:click={handleClick}> Click me</button>
+	<button>Fetch</button>
 </main>
 
+<!-- ### Style ########### -->
 <style>
 	main {
 		/* For alternating dark and light blocks */
 		background-color: var(--background-color);
+	}
+	article > header > h3 {
+		margin-bottom: 0;
+	}
+
+	.currencies {
+		display: grid;
+		grid-template-columns: auto 100px;
+		grid-column-gap: calc(var(--block-spacing-horizontal) * 3);
+		align-items: center;
+	}
+	.small-rates {
+		display: grid;
+		font-size: 0.8em;
 	}
 </style>
---
+++ src/routes/pico/Unused.svelte
@@ -0,0 +1,123 @@
+<main class="grid">
+	<aside data-theme="dark" class="closed-on-mobile">
+		<nav>
+			<p>Table of content</p>
+			<details>
+				<summary>Getting started</summary>
+				<ul>
+					<li><a href="./" id="start-link" class="secondary">Usage</a></li>
+					<li><a href="./themes.html" id="themes-link" class="secondary">Themes</a></li>
+					<li>
+						<a href="./customization.html" id="customization-link" class="secondary"
+							>Customization</a
+						>
+					</li>
+					<li>
+						<a href="./classless.html" id="classless-link" class="secondary">Class-less version</a>
+					</li>
+					<li><a href="./rtl.html" id="rtl-link" class="secondary">RTL</a></li>
+				</ul>
+			</details>
+
+			<details>
+				<summary>Layout</summary>
+				<ul>
+					<li><a href="./containers.html" id="containers-link" class="secondary">Containers</a></li>
+					<li><a href="./grid.html" id="grid-link" class="secondary">Grid</a></li>
+					<li>
+						<a href="./scroller.html" id="scroller-link" class="secondary">Horizontal scroller</a>
+					</li>
+				</ul>
+			</details>
+			<details>
+				<summary>Elements</summary>
+				<ul>
+					<li><a href="./typography.html" id="typography-link" class="secondary">Typography</a></li>
+					<li><a href="./buttons.html" id="buttons-link" class="secondary">Buttons</a></li>
+					<li><a href="./forms.html" id="forms-link" class="secondary">Forms</a></li>
+					<li><a href="./tables.html" id="tables-link" class="secondary">Tables</a></li>
+				</ul>
+			</details>
+			<details>
+				<summary>Components</summary>
+				<ul>
+					<li><a href="./accordions.html" id="accordions-link" class="secondary">Accordions</a></li>
+					<li><a href="./cards.html" id="cards-link" class="secondary">Cards</a></li>
+					<li><a href="./dropdowns.html" id="dropdowns-link" class="secondary">Dropdowns</a></li>
+					<li><a href="./modal.html" id="modal-link" class="secondary">Modal</a></li>
+					<li><a href="./navs.html" id="navs-link" class="secondary">Navs</a></li>
+					<li><a href="./progress.html" id="progress-link" class="secondary">Progress</a></li>
+				</ul>
+			</details>
+			<details>
+				<summary>Utilities</summary>
+				<ul>
+					<li><a href="./loading.html" id="loading-link" class="secondary">Loading</a></li>
+					<li><a href="./tooltips.html" id="tooltips-link" class="secondary">Tooltips</a></li>
+				</ul>
+			</details>
+			<details>
+				<summary>Extend</summary>
+				<ul>
+					<li>
+						<a href="./we-love-classes.html" id="we-love-classes-link" class="secondary"
+							>We love .classes</a
+						>
+					</li>
+				</ul>
+			</details>
+		</nav>
+		<script>
+			const activeLink = document.querySelector(`aside a#${props.active}`);
+			const parentAccordion = activeLink.closest('details');
+			activeLink.setAttribute('aria-current', 'page');
+			parentAccordion.setAttribute('open', 'true');
+		</script>
+	</aside>
+
+	<div role="document">
+		<h1>Accordions</h1>
+		<details>
+			<summary>Accordion 1</summary>
+			<p>…</p>
+		</details>
+
+		<details open>
+			<summary>Accordion 2</summary>
+			<ul>
+				<li>…</li>
+				<li>…</li>
+			</ul>
+		</details>
+
+		<h2>Accordion with role button</h2>
+		<!-- Primary -->
+		<details>
+			<summary role="button">Accordion 1</summary>
+			<p>…</p>
+		</details>
+
+		<!-- Secondary -->
+		<details>
+			<summary role="button" class="secondary">Accordion 2</summary>
+			<p>…</p>
+		</details>
+
+		<!-- Contrast -->
+		<details>
+			<summary role="button" class="contrast">Accordion 3</summary>
+			<p>…</p>
+		</details>
+	</div>
+</main>
+
+<style>
+	main {
+		grid-template-columns: 200px auto;
+		grid-column-gap: calc(var(--block-spacing-horizontal) * 3);
+	}
+	aside {
+		background-color: var(--background-color);
+		padding: var(--block-spacing-horizontal) var(--block-spacing-horizontal);
+	}
+</style>
