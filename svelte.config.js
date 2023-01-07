e182f66a1e570eb1a3e77307365ed1e68ee156af
Mark Koudritsky <kamrik@gmail.com>
2023-01-07 19:35:31 +0100

    Change to SPA mode in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,31 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+// Original version
+/** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// SPA mode
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+		  fallback: 'index.html'
+		}),
+		prerender: { entries: [] }
+	  }
 };
+
 
 export default config;
